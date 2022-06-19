0d66d3b17fe48c47524443e37d9bfe016c318d9d
echo-vladimir <echo.vladimir.k@gmail.com>
2022-06-19 18:59:48 +0200

    Replaced the language selection element as a <Link> to the <select> element. Added auto-detection of the language. Added storing the selected language in cookies. Added graphic layout of the element

--- components/Language.jsx
+++ components/Language.jsx
@@ -1,23 +1,47 @@
-import Link from 'next/link'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 import styles from './Language.module.scss'
 
 export default function Language() {
-    const { locale, locales, asPath } = useRouter()
+    const router = useRouter()
+    const { locale, locales, asPath, pathname, query } = router
+
+    const changeLocale = (locale) => {
+        router.push({ pathname, query }, asPath, { locale })
+    }
+
+    const setCookie = (locale) => {
+        document.cookie = `NEXT_LOCALE=${locale};`
+    }
+
+    const langs = {
+        'en': 'English',
+        'pl': 'Polski',
+        'ru': 'Русский',
+        'ua': 'Українська',
+    }
 
     return (
-        <div>
-            {locales.map((l, i) => {
-                return (
-                    <span key={i} className={l === locale ? styles.selected : ''}>
-                        {' /'}
-                        <Link href={asPath} locale={l} className={styles.language}>
-                            {l}
-                        </Link>
-                    </span>
-                );
-            })}
+        <div className={styles.select}>
+            <select
+                className={styles.control}
+                name='language'
+                value={locale}
+                onChange={(e) => {
+                    e.preventDefault()
+                    changeLocale(e.target.value)
+                    setCookie(e.target.value)
+                }}>
+                {locales.map((localeItem, i) => {
+                    return (
+                        <option value={localeItem} key={i}>
+                            {langs[localeItem]}
+                        </option>
+                    );
+                })}
+            </select>
+            <span className={styles.focus}></span>
         </div>
     )
 }
