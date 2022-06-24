6f377e33e8e767af778c3dddb7dd8451a2c9373f
echo-vladimir <echo.vladimir.k@gmail.com>
2022-06-25 01:36:08 +0200

    add user hook for custom animation

---
+++ components/Effects/useAnimation/useAnimation.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from "react"
+import timeCalculationFunctions from "./renderTime"
+
+export default function useAnimation(type, duration, drawFn, deps) {
+    const frame = useRef()
+    const init = useRef(performance.now())
+    const last = useRef(performance.now())
+    const renderTime = timeCalculationFunctions[type]
+
+    const render = time => {
+        const now = performance.now()
+
+        let timePassed = time - init.current
+        let timer = timePassed / duration
+        if (timer > 1) timer = 1
+
+        let progress = renderTime(timer)
+        // console.log(`timePassed: ${Math.round(timePassed / 1000)} Секунд. | timer: ${timer} | progress: ${progress}`)
+        drawFn(progress)
+
+        if (timer < 1) {
+            last.current = now
+            frame.current = requestAnimationFrame(render)
+        }
+    }
+
+    useEffect(() => {
+        frame.current = requestAnimationFrame(render)
+        return () => cancelAnimationFrame(frame.current)
+    }, deps)
+}
