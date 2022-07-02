224c366e582007c9586a1ef51a8e5477eab23192
echo-vladimir <echo.vladimir.k@gmail.com>
2022-07-03 00:17:39 +0200

    add Layout preloading

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,17 @@
-import '../styles/globals.scss'
-import variables from '../styles/variables.module.scss'
+import dynamic from 'next/dynamic'
 
-import Layout from '../components/Layout'
+import '../styles/globals.scss'
+import styles from '../styles/utils.module.scss'
+
+const Layout = dynamic(() =>
+  import('../components/Layout'), {
+  loading: () => <p className={styles.preloader}>Loading...</p>,
+  ssr: false
+})
 
 function App({ Component, pageProps }) {
   return (
-    <Layout color={variables.primaryColor}>
+    <Layout>
       <Component {...pageProps} />
     </Layout>
   )
