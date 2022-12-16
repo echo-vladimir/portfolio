453d426a54a3c79844c54aac5670d10076094272
echo-vladimir <echo.vladimir.k@gmail.com>
2022-12-16 19:37:59 +0100

    <Language> component change to a more comprehensible <LanguageSwitcher>

---
+++ components/LanguageSwitcher.jsx
@@ -0,0 +1,59 @@
+import { useRouter } from "next/router"
+import Link from "next/link"
+
+import styles from "./LanguageSwitcher.module.scss"
+
+export default function LanguageSwitcher() {
+    const { locales, locale } = useRouter()
+
+    const aliases = {
+        "en": "Eng",
+        "ru": "Рус"
+    }
+
+    return (
+        <>
+            {
+                locales.map((oneLocale, i) => {
+                    return (
+                        <Language
+                            key={oneLocale}
+                            locale={oneLocale}
+                            className={
+                                (oneLocale === locale)
+                                    ? styles.selected
+                                    : undefined}>
+                            {aliases[oneLocale]}
+                        </Language>
+                    )
+                })
+            }
+        </>
+    )
+}
+
+const Language = ({ children, locale, className }) => {
+    const router = useRouter()
+    const { asPath, pathname, query } = router
+
+    const setLocale = (locale) => {
+        router.push({ pathname, query }, asPath, { locale })
+    }
+
+    const setCookie = (locale) => {
+        document.cookie = `NEXT_LOCALE=${locale};`
+    }
+
+    return (
+        <Link
+            className={className}
+            href={pathname}
+            onClick={(e) => {
+                e.preventDefault()
+                setLocale(locale)
+                setCookie(locale)
+            }}>
+            {children}
+        </Link>
+    )
+}
