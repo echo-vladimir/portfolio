45270b560b7f7b141d85524e1968594c18a0d57d
echo-vladimir <echo.vladimir.k@gmail.com>
2022-12-28 00:20:14 +0100

    update layout design

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,45 +1,44 @@
 import Link from "next/link"
 import Image from "next/image"
 import Date from "./Date"
-
 import styles from "./Card.module.scss"
 
-export default function Card({ id, date, title, tags, cover, description, size }) {
-
-    const truncate = (str, maxlength) =>
-        (str.length > maxlength) ? str.slice(0, maxlength - 1) + "…" : str
-
+export default function Card({ id, date, title, team, tags, flag, cover, description, size }) {
     return (
-        <section className={`${styles[size]}`}>
+        <section className={styles[size]}>
             <Link href={`/cases/${id}`}>
-                <Image
-                    src={cover}
-                    alt={description}
-                    fill
-                    lazy="true"
-                    placeholder="blur"
-                    blurDataURL={`/_next/image?url=${cover}&w=16&q=1`}
-                    quality={90}
-                    sizes="100vw"
-                    style={{
-                        objectFit: "cover",
-                    }}
-                />
-                <div className={styles.description}>
-                    <h1 className={styles.title}>{title}</h1>
-                    <div className={styles.date}>
+                <div className={styles["img-wrapper"]}>
+                    <Image
+                        src={cover}
+                        alt={description}
+                        fill
+                        lazy="true"
+                        placeholder="blur"
+                        blurDataURL={`/_next/image?url=${cover}&w=16&q=1`}
+                        quality={90}
+                        sizes="100vw, 100vh"
+                        style={{
+                            objectFit: "cover",
+                        }}
+                    />
+                </div>
+                <div className={styles.info}>
+                    <div className={styles.center}>
                         <Date dateString={date} />
+                        <h1 className={styles.title}>{title}</h1>
+                        <p>{description}</p>
                     </div>
                     <div className={styles.tags}>
                         {
                             tags.map((tag) =>
                                 <span key={tag}>
-                                    {` [ ` + tag + ` ] `}
+                                    {tag}
                                 </span>
                             )
                         }
                     </div>
-                    <p>{description}</p>
+                    <div className={styles.end}>
+                    </div>
                 </div>
             </Link>
         </section>
