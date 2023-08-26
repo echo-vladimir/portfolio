71cfe899faa02ef150d396252412cbe9e0ad6a45
echo-vladimir <echo.vladimir.k@gmail.com>
2023-08-26 23:13:21 +0100

    Add basic project preview cards with dynamic content and styling

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,71 +1,92 @@
-import { useContext } from "react"
-import { VisibilityContext } from "react-horizontal-scrolling-menu"
-import Link from "next/link"
-import Image from "next/image"
-import Date from "./Date"
-
-import styles from "./Card.module.scss"
+import { useContext } from "react";
+import { VisibilityContext } from "react-horizontal-scrolling-menu";
+import Link from "next/link";
+import Image from "next/image";
+import Date from "./Date";
+import styles from "./Card.module.scss";
+import useDeviceType from "../hooks/useDeviceType";
 
 export default function Card({
-    onClick, selected, itemId, id, date, title,
-    team, tags, flag, cover, description, size
+  id,
+  date,
+  title,
+  team,
+  tags,
+  country,
+  cover,
+  description,
+  size,
+  margin,
+  width,
 }) {
-    const visibility = useContext(VisibilityContext)
+  const device = useDeviceType();
+  const visibility = useContext(VisibilityContext);
 
-    return (
-        <section className={styles[size]}>
-            <Link
-                href={`/cases/${id}`}
-                onClick={() => onClick(visibility)}
-                tabIndex={0}>
-                <div className={styles["img-wrapper"]}>
-                    <Image
-                        src={cover}
-                        alt={description}
-                        fill
-                        lazy="true"
-                        placeholder="blur"
-                        blurDataURL={`/_next/image?url=${cover}&w=16&q=1`}
-                        quality={90}
-                        sizes="100vw, 100vh"
-                        style={{
-                            objectFit: "cover",
-                        }}
-                    />
-                </div>
-                <div className={styles["overlay-container"]}>
-                    <div className={styles.tags}>
-                        {
-                            tags.map((tag) =>
-                                <span key={tag}>
-                                    {tag}
-                                </span>
-                            )
-                        }
-                    </div>
-                    <div className={styles.main}>
-                        <Date dateString={date} />
-                        <h1>{title}</h1>
-                        <p>{description}</p>
-                        {/* <div>visible: {JSON.stringify(!!visibility.isItemVisible(itemId))}</div>
-                        <div>selected: {JSON.stringify(!!selected)}</div> */}
-                    </div>
-                    {
-                        (flag) &&
-                        <div className={styles.footer}>
-                            <p className={styles.highlight}>Worked in </p>
-                            <Image
-                                className={styles.flag}
-                                src={`/images/${flag}.webp`}
-                                alt={"poland flag"}
-                                height={18}
-                                width={18}
-                            />
-                            {(team) && <p> with <span className={styles.highlight}>{team}</span> team</p>}
-                        </div>
-                    }
-                </div>
-            </Link>
-        </section>
-    )
+  return (
+    <section
+      className={`${visibility && styles.visible} ${styles[size]}`}
+      style={{
+        display: "inline-block",
+        userSelect: "none",
+        width,
+        marginLeft: margin,
+        marginRight: margin,
+      }}
+      tabIndex={0}
+    >
+      <Link href="/cases/[id]" as={`/cases/${id}`}>
+        <div className={styles["img-wrapper"]}>
+          <Image
+            src={cover}
+            alt={description}
+            fill
+            lazy="true"
+            placeholder="blur"
+            blurDataURL={`/_next/image?url=${cover}&w=16&q=1`}
+            quality={90}
+            sizes="100vw, 100vh"
+            style={{
+              objectFit: "cover",
+            }}
+          />
+        </div>
+        <div className={styles["centred-container"]}>
+          <div className={styles.tags}>
+            {tags.map((tag) => (
+              <span key={tag}>{tag}</span>
+            ))}
+          </div>
+          {country && (
+            <div className={styles.footer}>
+              <Image
+                className={styles.country}
+                src={`/images/${country}.webp`}
+                alt={`The flag of the country for ${title}`}
+                height={18}
+                width={18}
+              />
+              {team && device !== "mobile" ? (
+                <p>
+                  {" "}
+                  Made with <span className={styles.highlight}>
+                    {team}
+                  </span>{" "}
+                  team
+                </p>
+              ) : (
+                <p>{country}</p>
+              )}
+            </div>
+          )}
+        </div>
+        <div className={styles["overlay-container"]}>
+          <div className={styles.main}>
+            <Date dateString={date} />
+            <h1>{title}</h1>
+            <p>{description}</p>
+          </div>
+        </div>
+      </Link>
+    </section>
+  );
 }
--- components/CardHero.jsx
+++ components/CardHero.jsx
@@ -1,35 +1,132 @@
-import { TypingEffect } from "./Effects"
+import Link from "next/link";
+import { useContext, useEffect, useState } from "react";
+import useDeviceType from "../hooks/useDeviceType";
+import styles from "./CardHero.module.scss";
+import CardStyles from "../components/Card.module.scss";
+import {
+  button,
+  icoGithub,
+  icoDribbble,
+  icoLinkedin,
+} from "../components/LinkButton.module.scss";
+import { TypingEffect } from "./Effects";
 
-import styles from "./CardHero.module.scss"
+export default function CardHero({ width, margin }) {
+  const deviceType = useDeviceType();
 
-export default function CardHero() {
-    return (
-        <>
-            <section className={styles.container}>
-                <div className={styles.content}>
-                    <h2>
-                        Hello there, my name is Volodymyr and I&rsquo;m
-                    </h2>
-                    <h1>
-                        Front-end Developer
-                    </h1>
-                    <h2>and</h2>
-                    <h1>
-                        UX/UI Designer
-                    </h1>
-                    <h3>
-                        ‹ UX · UI · Figma · TypeScript · JavaScript · React · Next · NodeJS ›
-                    </h3>
-                    <p className={styles.footer}>
-                        <a href="mailto:echo.vladimir.k@gmail.com">
-                            echo.vladimir.k@gmail.com
-                        </a>
-                    </p>
-                    <p className={styles.footer}>
-                        <TypingEffect string={`available for new projects`} speed={2500} />
-                    </p>
-                </div>
-            </section>
-        </>
-    )
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        userSelect: "none",
+        width,
+        marginLeft: margin,
+        marginRight: margin,
+      }}
+    >
+      <section className={styles.container}>
+        <div className={styles.content}>
+          {deviceType === "mobile" && (
+            <p>
+              <Link
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/echo-vladimir"
+              >
+                <span
+                  className={icoGithub}
+                  style={{
+                    width: "17px",
+                    height: "17px",
+                  }}
+                />
+              </Link>
+              <Link
+                target="_blank"
+                rel="noreferrer"
+                href="https://dribbble.com/echo-vladimir"
+              >
+                <span
+                  className={icoDribbble}
+                  style={{ width: "18px", height: "18px", marginLeft: "18px" }}
+                />
+              </Link>{" "}
+              <Link
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/echo-vladimir"
+              >
+                <span
+                  className={icoLinkedin}
+                  style={{ width: "18px", height: "18px", marginLeft: "18px" }}
+                />
+              </Link>
+            </p>
+          )}
+          <h2>Hello there, my name is ‹Volodymyr /› and I&rsquo;m</h2>
+          <h1>Front-end Developer</h1>
+          <h3>&</h3>
+          <h1>UX/UI Designer</h1>
+          <h3>UX · UI · Figma · TypeScript · JS · NodeJS · Next · React</h3>
+          <p className={styles.footer}>
+            <a href="mailto:echo.vladimir.k@gmail.com">
+              echo.vladimir.k@gmail.com
+            </a>
+          </p>
+          <div className={styles.footer}>
+            {isClient ? (
+              <TypingEffect
+                string={`available for new projects`}
+                speed={5500}
+              />
+            ) : (
+              " "
+            )}
+          </div>
+        </div>
+      </section>
+      {deviceType === "desktop" && (
+        <div>
+          <div className={CardStyles["XS"]}>
+            <Link
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/echo-vladimir"
+              className={button}
+            >
+              <span className={icoGithub} />
+              <p>Github</p>
+            </Link>
+          </div>
+          <div className={CardStyles["XS"]}>
+            <Link
+              target="_blank"
+              rel="noreferrer"
+              href="https://dribbble.com/echo-vladimir"
+              className={button}
+            >
+              <span className={icoDribbble} />
+              <p>Dribbble</p>
+            </Link>
+          </div>
+          <div className={CardStyles["XS"]}>
+            <Link
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/echo-vladimir"
+              className={button}
+            >
+              <span className={icoLinkedin} />
+              <p>LinkedIn</p>
+            </Link>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
